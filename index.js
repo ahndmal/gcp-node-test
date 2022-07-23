5d1eed4d8f13ef9a20d1cbc54b2ebce6111c856a
andrii <quadr988@gmail.com>
2022-07-23 20:18:51 +0300

    datastore

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-import { Pool } from "pg";
+import * as pg from "pg";
 import "dotenv/config"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import { Datastore } from "@google-cloud/datastore";
 
 export function helloWorld(req, res) {
+  // Creates a client
   //   let message = req.query.message || req.body.message || "Hello World!";
   // let data = await getComments();
-  // resp
-  getComments().then((data) => res.status(200).send(data.rows));
+
+  saveMessage().then(() => {
+    // getComments().then((data) => res.status(200).send(data.rows));
+    res.status(200).send("test");
+  });
+
   // res.status(200).send(data.rows);
 }
 
 async function getComments() {
-  const pool = new Pool({
+  const pool = new pg.Pool({
     user: DB_USER,
     database: DB_NAME,
     password: DB_PASS,
@@ -25,4 +31,29 @@ async function getComments() {
   return data;
 }
 
-getComments().then((data) => console.log(data.rows));
+async function saveMessage() {
+  const datastore = new Datastore();
+  const kind = "MessageRecord";
+
+  // The name/ID for the new entity
+  const name = "sampletask1";
+
+  // The Cloud Datastore key for the new entity
+  const msgKey = datastore.key([kind, null]);
+  // const taskKey = datastore.key;
+
+  // Prepares the new entity
+  const msgRec = {
+    key: msgKey,
+    data: {
+      EventTyme: Date.now(),
+      From: "Andrii NodeJS",
+      Message: "test message",
+    },
+  };
+
+  await datastore.save(msgRec);
+  console.log(`Saved ${msgRec.key.name}: ${msgRec.EventTyme}`);
+}
+
+// getComments().then((data) => console.log(data.rows));
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
+## NodeJS GCP cloud function
+
+primitive NodeJS GCP function for fast deploy and test of GCP cloud.
+
 ```shell
 gcloud functions deploy node2 --trigger-http --runtime=nodejs16 --entry-point=helloWorld --allow-unauthenticated --memory=256MB
 ```
